4243d9538d6716e3dc5c87f8ce4a505f43ef243e
Versart <wesleyversart@gmail.com>
2023-01-14 22:47:48 -0300

    adicionando a funcionalidade de empilhar e desempilhar a pilha

--- src/main/java/org/example/Pilha.java
+++ src/main/java/org/example/Pilha.java
@@ -1,11 +1,36 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Pilha {
+    public String[] elementos;
+    private int size;
+
+    public Pilha() {
+        this.size = 0;
+        elementos = new String[10];
+    }
     public boolean isEmpty() {
-        return true;
+        return this.size == 0;
     }
 
-    public int size() {
-        return 0;
+    public int getSize() {
+        return this.size;
+    }
+
+    public void push(String elemento) {
+        elementos[size] = elemento;
+        this.size++;
+    }
+
+    public String peek() {
+        return elementos[size - 1];
+    }
+
+    public String pop() {
+        String elementoRetirado = elementos[size - 1];
+        elementos[size -1] = null;
+        size--;
+        return elementoRetirado;
     }
 }
--- src/test/java/org/example/PilhaTeste.java
+++ src/test/java/org/example/PilhaTeste.java
@@ -10,6 +10,29 @@ public class PilhaTeste {
     public void pilhaDeveSerVazia() {
         Pilha pilha = new Pilha();
         assertTrue(pilha.isEmpty());
-        assertEquals(0,pilha.size());
+        assertEquals(0,pilha.getSize());
+    }
+
+    @Test
+    public void deveEmpilharUmElementoNaPilha() {
+        Pilha pilha = new Pilha();
+        pilha.push("Maria");
+        assertFalse(pilha.isEmpty());
+        assertEquals(1,pilha.getSize());
+        assertEquals("Maria",pilha.peek());
+    }
+
+    @Test
+    public void deveEmpilharDoisElementoEDesempilharUmElemento() {
+        Pilha pilha = new Pilha();
+        pilha.push("Maria");
+        pilha.push("João");
+        assertFalse(pilha.isEmpty());
+        assertEquals(2,pilha.getSize());
+        assertEquals("João",pilha.peek());
+        String elementoRetirado = pilha.pop();
+        assertEquals(1,pilha.getSize());
+        assertEquals("Maria",pilha.peek());
+        assertEquals("João", elementoRetirado);
     }
 }
